5f7fbe441a986cfb2abe60b91c56c99eb93a86b5
David Symonds <dsymonds@gmail.com>
2023-07-30 19:15:40 +1000

    Add methods to query a controller's current state, and set its effect.

--- nanoleaf.go
+++ nanoleaf.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -23,6 +24,28 @@ func Connect(ip, authToken string) (*Controller, error) {
 		ip:        ip,
 		authToken: authToken,
 	}, nil
+}
+
+type State struct {
+	Name            string `json:"name"` // the native name, not what you've given it
+	Serial          string `json:"serialNo"`
+	FirmwareVersion string `json:"firmwareVersion"`
+
+	Effects struct {
+		Selected string   `json:"select"`
+		List     []string `json:"effectsList"`
+	} `json:"effects"`
+
+	// TODO: panelLayout, state
+}
+
+// State requests the state of the controller.
+func (c *Controller) State(ctx context.Context) (*State, error) {
+	var state State
+	if err := c.get(ctx, "/", &state); err != nil {
+		return nil, err
+	}
+	return &state, nil
 }
 
 // Off turns the controller off.
@@ -43,6 +66,34 @@ func (c *Controller) On(ctx context.Context) error {
 	}{}
 	req.On.Value = true
 	return c.put(ctx, "/state", req)
+}
+
+func (c *Controller) SetEffect(ctx context.Context, effect string) error {
+	req := struct {
+		Select string `json:"select"`
+	}{Select: effect}
+	return c.put(ctx, "/effects", req)
+}
+
+func (c *Controller) get(ctx context.Context, path string, dst interface{}) error {
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://"+c.ip+":16021/api/v1/"+c.authToken+path, nil)
+	if err != nil {
+		return fmt.Errorf("preparing HTTP request: %w", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("making HTTP request: %w", err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	// TODO: body to capture?
+	if resp.StatusCode >= 300 {
+		return fmt.Errorf("HTTP response %s", resp.Status)
+	}
+	if err != nil {
+		return fmt.Errorf("reading HTTP response body: %w", err)
+	}
+	return json.Unmarshal(body, dst)
 }
 
 func (c *Controller) put(ctx context.Context, path string, obj interface{}) error {
